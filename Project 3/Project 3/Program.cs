2530d6380475b959f0a8f61868a999b0dc41de9b
Rohill Nand (1000053700) <NANDR3@student.op.ac.nz>
2018-11-05 14:41:04 +1300

    Methods Area

--- Project 3/Project 3/Program.cs
+++ Project 3/Project 3/Program.cs
@@ -18,5 +18,106 @@ namespace Project_3
         {
             //Test
         }
+        public static void L4()
+        {
+            //Starting room
+        }
+
+        public static void L3()
+        {
+
+        }
+
+        public static void L2()
+        {
+
+        }
+
+        public static void K2()
+        {
+
+        }
+
+        public static void J2()
+        {
+
+        }
+        public static void J3()
+        {
+
+        }
+        public static void J4()
+        {
+
+        }
+        public static void J5()
+        {
+
+        }
+        public static void I5()
+        {
+
+        }
+        public static void H5()
+        {
+
+        }
+        public static void G5()
+        {
+
+        }
+        public static void G6()
+        {
+
+        }
+        public static void G7()
+        {
+
+        }
+        public static void G8()
+        {
+
+        }
+        public static void G9()
+        {
+            //Exit
+        }
+        public static void F5()
+        {
+
+        }
+        public static void E5()
+        {
+
+        }
+        public static void D5()
+        {
+
+        }
+        public static void D4()
+        {
+
+        }
+        public static void D6()
+        {
+
+        }
+        public static void D7()
+        {
+
+        }
+        public static void D8()
+        {
+
+        }
+        public static void C8()
+        {
+
+        }
+        public static void B8()
+        {
+
+        }
+
     }
 }
